e45dbe635b236926966c6b93ae11c9d7f78183e9
App Academy Student <patricksbrown1992@gmail.com>
2019-05-28 18:13:40 -0400

    update entities reducer with session reducer

--- frontend/reducers/entities/entitiesReducer.jsx
+++ frontend/reducers/entities/entitiesReducer.jsx
@@ -1,9 +1,10 @@
 import usersReducer from './usersReducer';
-
+import sessionReducer from './session_reducer';
 import {combineReducers} from 'redux';
 
 const entitiesReducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    session: sessionReducer
 });
 export default entitiesReducer;
 
