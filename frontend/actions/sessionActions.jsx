6a2a78e872a0f396889e21e322c12051c8216be5
App Academy Student <patricksbrown1992@gmail.com>
2019-05-28 18:05:10 -0400

    finish users reducer

--- frontend/actions/sessionActions.jsx
+++ frontend/actions/sessionActions.jsx
@@ -2,6 +2,7 @@ import * as APIUtil from '../util/sessionAPIUtil';
 
 export const RECEIVE_USER = 'RECEIVE_USER';
 export const REMOVE_USER = 'REMOVE_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 const receiveUser = (user) => ({
     type: RECEIVE_USER,
@@ -9,19 +10,20 @@ const receiveUser = (user) => ({
 });
 
 const removeUser = () => ({
-    type: REMOVE_USER
+    type: REMOVE_USER,
+   
 });
 
 export const signup = (user) => dispatch => (
-    APIUtil.signup(user).then(dispatch(receiveUser(user)))
+    APIUtil.signup(user).then(user => dispatch(receiveUser(user)))
 );
 
 export const login = (user) => dispatch => (
-    APIUtil.login(user).then(dispatch(receiveUser(user)))
+    APIUtil.login(user).then(user => dispatch(receiveUser(user)))
 );
 
 export const logout = () => dispatch => (
-    APIUtil.logout().then(dispatch(removeUser()))
+    APIUtil.logout().then(user => dispatch(removeUser()))
 );
 
 
--- frontend/reducers/entities/usersReducer.jsx
+++ frontend/reducers/entities/usersReducer.jsx
@@ -1,9 +1,10 @@
 import {RECEIVE_USER, REMOVE_USER} from '../../actions/sessionActions';
+import {merge} from 'lodash';
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type){
-
-
+        case RECEIVE_USER:
+            return merge({}, state, {[action.user.id]:action.user});
         default: 
             return state;
     }
