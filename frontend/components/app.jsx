e25cac6136f6cec8572af81bed3b4879180dcf94
App Academy Student <patricksbrown1992@gmail.com>
2019-05-29 12:13:43 -0400

    log in form skeleton

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
-import SplashForm from './splash/splashContainer';
+import SplashForm from './splash/splashForm';
+import LogInForm from './logInForm';
 const App = () => (
     <div>
- 
+        <header>
+            <h1 className = 'title'>NeverNote</h1>
+           
+        </header>
         
-        <h1>NeverNote</h1>
-        < SplashForm />
+       
+        <Switch>
+            <Route path='/login' component={LogInForm} />
+            <Route exact path='/' component={SplashForm} />
+        </Switch>
        
            
         
--- frontend/components/splash/splashForm.jsx
+++ frontend/components/splash/splashForm.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
-
+// import splash_main from '../../../app/assets/images/splash_main';
 
 const splashForm = () => (
-    <div>
-        <div className = "login-button">
+    <div className ='splash-buttons'>
+        <nav className = "login-button">
             <Link to='/login'>Log in</Link>
-        </div>
+        </nav>
             
-        <div className = "signup-button">
-            <Link to='/signup'>Sign Up</Link>
+        <nav className = "signup-button">
+            <Link to='/signup'>Sign up</Link>
+        </nav>
 
-        </div>
+       
     </div>
 );
 
