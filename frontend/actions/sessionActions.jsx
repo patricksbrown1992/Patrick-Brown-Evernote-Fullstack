806d2c7e47a6846ae85c9a0d25e003cf4a66255f
App Academy Student <patricksbrown1992@gmail.com>
2019-05-29 08:40:06 -0400

    add errors actions to session actions

--- frontend/actions/sessionActions.jsx
+++ frontend/actions/sessionActions.jsx
@@ -2,7 +2,7 @@ import * as APIUtil from '../util/sessionAPIUtil';
 
 export const RECEIVE_USER = 'RECEIVE_USER';
 export const REMOVE_USER = 'REMOVE_USER';
-export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 
 const receiveUser = (user) => ({
     type: RECEIVE_USER,
@@ -13,17 +13,26 @@ const removeUser = () => ({
     type: REMOVE_USER,
    
 });
+const receiveErrors = errors => ({
+    type: RECEIVE_ERRORS,
+    errors
+});
 
 export const signup = (user) => dispatch => (
-    APIUtil.signup(user).then(user => dispatch(receiveUser(user)))
+    APIUtil.signup(user).then(user => (dispatch(receiveUser(user))
+    ), err => (dispatch(receiveErrors(err.responseJSON))
+    ))
 );
 
 export const login = (user) => dispatch => (
-    APIUtil.login(user).then(user => dispatch(receiveUser(user)))
+    APIUtil.login(user).then(user => dispatch(receiveUser(user))
+    ), (err => (dispatch(receiveErrors(err.responseJSON))
+    ))
 );
 
 export const logout = () => dispatch => (
-    APIUtil.logout().then(user => dispatch(removeUser()))
+    APIUtil.logout().then(user => dispatch(removeUser())
+    )
 );
 
 
