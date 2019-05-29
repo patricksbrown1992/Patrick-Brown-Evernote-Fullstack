ba03ea0c76140ada8951c2b2cc96cf1bc6f72fcf
App Academy Student <patricksbrown1992@gmail.com>
2019-05-29 17:59:09 -0400

    final push for day

--- frontend/components/logInForm.jsx
+++ frontend/components/logInForm.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const logInForm = () => (
-    
-    <div className ='log-in-form'>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Evernote_Icon.png/220px-Evernote_Icon.png" alt=""/>
-        <h1 className = 'log-in-evernote'>NeverNote</h1>
-        <h3>Remember everything important.</h3>
-        <input className="log-in-email-input" placeholder= 'Email address or username' type="text" name="submitter" id=""/>
-        
-        <div className='enter-username'>
+    <div class='outerdiv'>
+        <div className ='log-in-form'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Evernote_Icon.png/220px-Evernote_Icon.png" alt=""/>
+            <h1 className = 'log-in-evernote'>NeverNote</h1>
+            <h3>Remember everything important.</h3>
+            <input className="log-in-email-input" placeholder= 'Email address or username' type="text" name="submitter" id=""/>
+            
+            <div className='enter-username'>
 
-            <Link to='/loginwithpassword'>Continue</Link>
+                <Link to='/loginwithpassword'>Continue</Link>
+            </div>
+            <h3>Remember me for 30 days</h3>
+            <h3>Don't have an account?</h3>
+            <div className='redirect-to-login-signup'>
+                <Link to='/signup'>Create account</Link>
+            </div>
         </div>
-        <h3>Remember me for 30 days</h3>
-        <h3>Don't have an account?</h3>
-        <div className='redirect-to-login-signup'>
-            <Link to='/signup'>Create account</Link>
-        </div>
+
     </div>
     
 )
--- frontend/components/signUpForm.jsx
+++ frontend/components/signUpForm.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUpForm = () => (
-    <div className='sign-up-form'>
-    
+    <div className = 'outerdiv'>
+        <div className='sign-up-form'>
+        
+                
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Evernote_Icon.png/220px-Evernote_Icon.png" alt=""/>
+                <h1 className='log-in-evernote'>NeverNote</h1>
+                <h3>Remember everything important.</h3>
+             
+                <input className="sign-up-email" type="text" placeholder='email' name="user-name-submitter" id="" />
+                <input className = "sign-up-password" type="password" placeholder='password' name="password-submitter" id=""/>
+                
+                <div className='sign-up-continue'>
 
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Evernote_Icon.png/220px-Evernote_Icon.png" alt=""/>
-            <h1 className='log-in-evernote'>NeverNote</h1>
-            <h3>Remember everything important.</h3>
-            <input className="sign-up-email" type="text" placeholder='email' name="user-name-submitter" id="" />
-            <input className = "sign-up-password" type="password" placeholder='password' name="password-submitter" id=""/>
-            
-            <div className='sign-up-continue'>
-
-                <Link to='/username'>Continue</Link>
-            </div>
-            <h3>By creating an account, you are agreeing to our Terms of Service and Privacy Policy.</h3>
-            <h3>Already have an account?</h3>
-            <div className='redirect-to-login-signup'>
-                <Link to='/login'>Sign In</Link>
-            </div>
-       
+                    <Link to='/username'>Continue</Link>
+                </div>
+                <p>By creating an account, you are agreeing to our Terms of Service and Privacy Policy.</p>
+                <h3>Already have an account?</h3>
+                <div className='redirect-to-login-signup'>
+                    <Link to='/login'>Sign In</Link>
+                </div>
+        
+        </div>
     </div>
 
 )
