42ac09b11d9f4274cd28ce38c4999de16d1b569a
App Academy Student <patricksbrown1992@gmail.com>
2019-05-29 06:03:48 -0400

    move session reducer from entities to root reducer

--- frontend/reducers/entities/entitiesReducer.jsx
+++ frontend/reducers/entities/entitiesReducer.jsx
@@ -1,10 +1,10 @@
 import usersReducer from './usersReducer';
-import sessionReducer from './session_reducer';
+
 import {combineReducers} from 'redux';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    session: sessionReducer
+    
 });
 export default entitiesReducer;
 
--- frontend/reducers/rootReducer.jsx
+++ frontend/reducers/rootReducer.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import entitiesReducer from './entities/entitiesReducer';
+import sessionReducer from './session_reducer';
 
 const rootReducer = combineReducers({
-    entities: entitiesReducer
+    entities: entitiesReducer,
+    session: sessionReducer
 });
 
 export default rootReducer;
