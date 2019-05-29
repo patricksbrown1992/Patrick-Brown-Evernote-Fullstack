163e3449cbb752d363fd90bdaab7cb8b25b0e02d
App Academy Student <patricksbrown1992@gmail.com>
2019-05-29 08:15:45 -0400

    download logger and thunk and add store to entry file

--- frontend/nevernote.jsx
+++ frontend/nevernote.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import * as APIUtil from './util/sessionAPIUtil';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     window.login = APIUtil.login;
     window.signup = APIUtil.signup;
     window.logout = APIUtil.logout;
-    ReactDOM.render(<Root />, root);
+    const store = configureStore();
+    ReactDOM.render(<Root store={store}/>, root);
 });
