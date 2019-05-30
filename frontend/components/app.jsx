d02570613ab7062563e828a3a688a05a927bd551
App Academy Student <patricksbrown1992@gmail.com>
2019-05-30 16:45:45 -0400

    fixed merge problems

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,15 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
 import SplashForm from './splash/splashForm';
-<<<<<<< HEAD
-import LogInForm from './logInForm';
-import SignUpForm from './signUp/signUpContainer';
-=======
 import LogInForm from './logIn/logInContainer';
 import SignUpForm from './signUp/signUpContainer';
-import UsernameForm from './username/usernameForm';
->>>>>>> user_log_in
 import { Authorized, ProtectedRoute } from '../util/routeUtil';
+import UsernameForm from './username/usernameForm';
 const App = () => (
     <div>
 
--- frontend/components/signUp/signUpContainer.jsx
+++ frontend/components/signUp/signUpContainer.jsx
@@ -5,11 +5,8 @@ import { signup } from '../../actions/sessionActions';
 const msp = (state) => ({
     errors: state.errors
 });
-<<<<<<< HEAD
 
-=======
  
->>>>>>> user_log_in
 const mdp = (dispatch) => ({
     signup: (user) => dispatch(signup(user))
 });
