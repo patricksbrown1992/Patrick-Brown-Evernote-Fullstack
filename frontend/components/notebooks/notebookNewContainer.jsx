44ba0477b1d36a08559c1360fb2f940566ca4e37
App Academy Student <patricksbrown1992@gmail.com>
2019-06-04 17:15:26 -0400

    functional new modal unstyled

--- frontend/components/notebooks/notebookNewContainer.jsx
+++ frontend/components/notebooks/notebookNewContainer.jsx
@@ -1,4 +1,12 @@
 import { connect } from 'react-redux';
 import NotebookNewForm from './notebookNewForm';
+import {createNotebook} from '../../actions/notebookActions';
+const msp = state => ({
+    user: state.entities.user,
+});
 
+const mdp = dispatch => ({
+   createNotebook: (notebook) => (dispatch(createNotebook(notebook))) 
+});
 
+export default connect(msp, mdp)(NotebookNewForm);
--- frontend/components/notebooks/notebookNewForm.jsx
+++ frontend/components/notebooks/notebookNewForm.jsx
@@ -1,15 +1,44 @@
 import React from 'react'; 
 
 
-const NotebookNewForm = () => {
-    debugger
-    return (
+class NotebookNewForm extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {name: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange() {
+        // debugger
+        return (e) => {
+            this.setState({ name: e.target.value });
+        };
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        const name = this.state.name;
+        const user_id = Object.values(this.props.user)[0].id;
+        // debugger
+        this.props.createNotebook({name, user_id});
+    }
     
-    <div>
-        <h1>New Notebook</h1>
-    </div>
-    )
+
+    render (){
+        return(
+        <div className='new-notebook-modal'>
+            <h1>Create new notebook</h1>
+                <h3>Notebooks are useful for grouping notes around a common topic. They can be private or shared.</h3>
+                <h5>Name</h5>
+                <input type="text" value={this.state.name} onChange={this.handleChange()} placeholder='Notebook name'/>
+                <form onSubmit={this.handleSubmit}>
+                    <button type='submit'>Continue</button>
+                </form>
+        </div>
+        )
+    }
 }
 
 
