474513e27ef705979f1d5c03a122f5f0987d0a81
App Academy Student <patricksbrown1992@gmail.com>
2019-06-04 08:13:09 -0400

    fixes made to note show

--- frontend/components/notes/noteShowForm.jsx
+++ frontend/components/notes/noteShowForm.jsx
@@ -4,16 +4,30 @@ import React from 'react';
 class NoteShowForm extends React.Component {
     constructor(props){
         super(props);
+        // this.state = {selected: 1};
+        // this.updateSelected = this.updateSelected.bind(this);
     }
 
+    // updateSelected(id) {
+    //     return () => {
+    //         this.setState({ selected: id });
+    //     };
+    // }
+
     render(){
-        return (
-            <div className="right-nav">
-                <h1>First Note</h1>
-                <h3>This is the first note</h3>
-                <h3>Really the first note</h3>
-            </div>
-        );
+        // let note;
+        // debugger
+        // if(!note){
+        //     note = Object.values(this.props.notes)[0];
+        // } else {
+            
+            return (
+                <div className="right-nav">
+                    <h1>First note</h1>
+                    <h3>this is the first note</h3>
+                </div>
+            );
+        // }
     }
 
 }
