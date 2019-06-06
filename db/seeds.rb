b8a26873bc222f4cba1956ab1b1b08b73eb48d18
App Academy Student <patricksbrown1992@gmail.com>
2019-06-06 19:29:39 -0400

    fixes for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 admin = User.create(email: 'admin@admin.com', password: '123456')
