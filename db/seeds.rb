2bdea11bc6e5a70adce4d86ab08f265d8c97d4a7
App Academy Student <patricksbrown1992@gmail.com>
2019-06-07 09:58:22 -0400

    seed database for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-admin = User.create(email: 'admin@admin.com', password: '123456')
+Notebook.destroy_all
+Note.destroy_all
+u1 = User.create!(email: 'admin@admin.com', password: '123456')
+u2 = User.create!(email: 'buzz@lightyear.com', password: '123456')
+nb1 = Notebook.create!(name: 'demo notebook', user_id: u1.id)
+nb3 = Notebook.create!(name: 'To Infinity!', user_id: u2.id)
+n1 = Note.create!(title: 'And Beyond!', body: 'And Beyond!', notebook_id: nb3.id)
+
