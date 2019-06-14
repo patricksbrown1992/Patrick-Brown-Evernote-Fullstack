4a5a06a934eb61004f3369b1e34ed2b5f7707cb7
patrickbrown <patricksbrown1992@gmail.com>
2019-06-14 15:54:38 -0400

    tag controller minus index

---
+++ app/controllers/api/tags_controller.rb
@@ -0,0 +1,49 @@
+class TagsController < ApplicationController
+    before_action :require_logged_in
+    def index
+       
+    end
+
+    def show
+        @tag = Tag.find(params[:id])
+        if @tag
+            render json: @note, status: 200
+        else
+            render json: @tag.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @tag = Tag.find(params[:id])
+        if @tag.update(tag_params)
+            render json: @tag, status: 200
+        else
+            render json: @note.errors.full_messages, status; 422 
+        end
+    end
+
+    def create
+        @tag = Tag.new(tag_params)
+        if @tag.save
+            render json: @tag, status: 200
+        else
+            render json: @tag.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        if @tag.destroy
+            render json: @tag, status: 200
+        else
+            render json: @tag.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def tag_params
+        params.require(:tag).permit(:note_id, :name)
+    end
+
+end
