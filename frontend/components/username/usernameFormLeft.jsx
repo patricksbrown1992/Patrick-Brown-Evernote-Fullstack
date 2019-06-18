1b74b14e553bd61ae43293e73a7234a6a6aff476
patrickbrown <patricksbrown1992@gmail.com>
2019-06-18 17:45:27 -0400

    functional and stylized left nav notebook index with working caret

--- frontend/components/username/usernameFormLeft.jsx
+++ frontend/components/username/usernameFormLeft.jsx
@@ -7,9 +7,18 @@ class usernameFormLeft extends React.Component {
         super(props);
         this.handleSubmitLogOut = this.handleSubmitLogOut.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.state = { loaded: false, body: '' };
+        this.state = { body: '', selected: false };
+        this.updateSelected = this.updateSelected.bind(this)
 
     }
+
+
+    updateSelected() {
+        
+        this.setState({ selected: true });
+        
+    }
+
 
     handleChange(){
         return (e) => {
@@ -38,8 +47,24 @@ class usernameFormLeft extends React.Component {
 
 
     render() {
+        let notebooks;
+        let caret;
+        debugger
+        if(this.state.selected){
+            debugger
+            caret = "fas fa-caret-down";
+            notebooks = this.props.notebooks.map(notebook => (
+                <li key={notebook.id}>
+                    <div className="username-left-notebook-index"><i className="fas fa-book"></i>{notebook.name}</div>
+                </li>
+            ))
+        } else {
+            debugger
+            notebooks = '';
+            caret = "fas fa-caret-right";
+        }
 
-        //    debugger
+      
         return (
 
             <div className="left-nav">
@@ -56,7 +81,10 @@ class usernameFormLeft extends React.Component {
                     <span onClick={this.handleSubmitNewNote(this.props.notebook)} className="new-note"><i className="fas fa-plus-circle fa-2x"></i><button type='submit'>New Note</button></span>
                     <li><i className="fas fa-caret-right"></i><i className="fas fa-star"></i>Shortcuts</li>
                     <li><Link to='/allnotes'><i className="fas fa-sticky-note"></i>All Notes</Link></li>
-                    <li><i className="fas fa-caret-right"></i><Link to='/notebooks'><i className="fas fa-book"></i>Notebooks</Link></li>
+                    <li><i className={caret} onClick={this.updateSelected}></i><Link to='/notebooks'><i className="fas fa-book"></i>Notebooks</Link></li>
+                    <ul>
+                        {notebooks}
+                    </ul>
                     <li><i className="fas fa-user-friends"></i>Shared with Me</li>
                     <li><Link to='/tags'><i className="fas fa-tag"></i>Tags</Link></li>
                     <li><i className="fas fa-trash"></i>Trash</li>
--- frontend/components/username/usernameLeftContainer.jsx
+++ frontend/components/username/usernameLeftContainer.jsx
@@ -2,15 +2,20 @@ import UsernameFormLeft from './usernameFormLeft';
 import { connect } from 'react-redux';
 import { logout } from '../../actions/sessionActions';
 import {openAddNoteModal} from '../../actions/modalActions';
+import { getNotebooks } from '../../actions/notebookActions';
+import { clearNotebooks} from '../../actions/notebookActions';
 
 
 const msp = state => ({
-    user: state.entities.user[state.session.id]
+    user: state.entities.user[state.session.id],
+    notebooks: Object.values(state.entities.notebooks)
 });
 
 const mdp = dispatch => ({
     logout: () => dispatch(logout()),
-    addNote: entity => dispatch(openAddNoteModal(entity))
+    addNote: entity => dispatch(openAddNoteModal(entity)),
+    getNotebooks: (user) => dispatch(getNotebooks(user)),
+    clearNotebooks: () => dispatch(clearNotebooks())
    
 });
 
