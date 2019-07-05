3970b514f166ab8d61c70f88fb5b5db9027f90af
patrickbrown <patricksbrown1992@gmail.com>
2019-07-05 12:14:37 -0400

    properly stylized new note modal

--- frontend/components/notes/noteNewForm.jsx
+++ frontend/components/notes/noteNewForm.jsx
@@ -31,27 +31,52 @@ class NoteNewForm extends React.Component {
 
        
     render(){
-    return (
-        <div className='new-note-modal'>
-            <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-            <h1>Create new Note</h1>
-           
-            <h5>Title</h5>
-            <span>
-                <input type="text" value={this.state.title} onChange={this.handleChange('title')} placeholder='Note Title' />
-            </span>
-            <h5>Body</h5>
-            <span>
-                <input type="text" value={this.state.body} onChange={this.handleChange('body')} placeholder='Note Body' />
-            </span>
-            <br/>
-            <form onSubmit={this.handleSubmit}>
-                <button  type='submit'>Continue</button>
+        if (this.state.title.length < 1 || this.state.body.length < 1){
+            return (
+                <div className='new-note-modal'>
+                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
+                    <h1>Create new Note</h1>
 
-            </form>
+                    <h5>Title</h5>
+                    <span>
+                        <input type="text" value={this.state.title} onChange={this.handleChange('title')} placeholder='Note Title' />
+                    </span>
+                    <h5>Body</h5>
+                    <span>
+                        <input type="text" value={this.state.body} onChange={this.handleChange('body')} placeholder='Note Body' />
+                    </span>
+                    <br />
+                    <form onSubmit={this.handleSubmit}>
+                        <button className="invalid" type='submit'>Done</button>
 
-        </div>
-        )
+                    </form>
+
+                </div>
+            )
+        } else {
+
+            return (
+                <div className='new-note-modal'>
+                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
+                    <h1>Create new Note</h1>
+                   
+                    <h5>Title</h5>
+                    <span>
+                        <input type="text" value={this.state.title} onChange={this.handleChange('title')} placeholder='Note Title' />
+                    </span>
+                    <h5>Body</h5>
+                    <span>
+                        <input type="text" value={this.state.body} onChange={this.handleChange('body')} placeholder='Note Body' />
+                    </span>
+                    <br/>
+                    <form onSubmit={this.handleSubmit}>
+                        <button className="valid" type='submit'>Done</button>
+        
+                    </form>
+        
+                </div>
+                )
+        }
     }
 }
 
