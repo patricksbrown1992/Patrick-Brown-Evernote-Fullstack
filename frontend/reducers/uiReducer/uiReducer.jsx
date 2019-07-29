8e3f6aad3711248744aabc3b5662e7ee6eda0fe3
patrickbrown <patricksbrown1992@gmail.com>
2019-07-29 14:15:54 -0400

    ui reducer update

--- frontend/reducers/uiReducer/uiReducer.jsx
+++ frontend/reducers/uiReducer/uiReducer.jsx
@@ -3,11 +3,13 @@ import CheckUserReducer from '../checkUser/checkUserReducer';
 import modalReducer from './modalReducer';
 import ShortcutReducer from './ShortcutReducer';
 import SearchReducer from './searchReducer';
+import TagTriage from './tagTriageReducer';
 
 const uiReducer = combineReducers({
     verified: CheckUserReducer,
     modal: modalReducer,
     shortcut: ShortcutReducer,
-    search: SearchReducer
+    search: SearchReducer,
+    Triage: TagTriage
 });
 export default uiReducer;
