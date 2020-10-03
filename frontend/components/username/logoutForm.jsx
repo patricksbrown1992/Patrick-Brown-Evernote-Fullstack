44ca1161dd8af6c9f8bc62017c49a658630154f0
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 00:14:25 -0400

    converted logout form to React hook

--- frontend/components/username/logoutForm.jsx
+++ frontend/components/username/logoutForm.jsx
@@ -1,36 +1,29 @@
 import React from 'react';
 
 
-class LogoutForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.handleSubmitLogOut = this.handleSubmitLogOut.bind(this);
-    }
+const LogoutForm = (props) => {
 
-    handleSubmitLogOut(e) {
-        
+    function handleSubmitLogOut(e){
         e.preventDefault();
-        this.props.logout();
-        this.props.closeModal();
+        props.logout();
+        props.closeModal();
     }
 
+    return(
 
-    render(){
-        
-        return(
-
-            <div className="logout-modal">
-                <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                <br/>
-                <br/>
+        <div className="logout-modal">
+            <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
+            <br/>
+            <br/>
 
 
-                <button onClick={this.handleSubmitLogOut} type='submit'>Sign Out {this.props.user.email}</button>
-                
-             
-            </div>
-        )
-    }
+            <button onClick={handleSubmitLogOut} type='submit'>Sign Out {props.user.email}</button>
+            
+         
+        </div>
+    )
 }
+
+
 
 export default LogoutForm;
