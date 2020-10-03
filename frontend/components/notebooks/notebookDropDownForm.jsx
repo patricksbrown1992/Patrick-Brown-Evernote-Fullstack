c744d6537668a2ce36ad6d170df13619c1b12b17
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 00:50:01 -0400

    convert notebook drop down to hook

--- frontend/components/notebooks/notebookDropDownForm.jsx
+++ frontend/components/notebooks/notebookDropDownForm.jsx
@@ -1,87 +1,56 @@
 import React from 'react';
 
-class NotebookDropDownForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.handleSubmitDelete = this.handleSubmitDelete.bind(this);
-        this.handleSubmitEdit = this.handleSubmitEdit.bind(this);
-        this.handleSubmitShortcut = this.handleSubmitShortcut.bind(this);
-    }
+const NotebookDropDownForm = (props) => {
+   
     
-    handleSubmitDelete(entity) {
-
-        return (e) => {
-            e.preventDefault();
-            this.props.notebookDelete(entity);
-        };
+    function handleSubmitDelete(e) {
+        e.preventDefault();
+        props.notebookDelete(props.notebook);    
     };
-    handleSubmitEdit(entity) {
-
-        return (e) => {
-            e.preventDefault();
-            this.props.editModal(entity);
-        };
-    };
-    handleSubmitShortcut(entity){
-        return(e) => {
-            e.preventDefault();
-            const name = entity.name;
-            const user_id = entity.user_id;
-            const shortcut = !entity.shortcut;
-            const id = entity.id;
-            this.props.closeModal();
-            this.props.updateNotebook({id: id, name: name, user_id: user_id, shortcut: shortcut})
-        };
+    function handleSubmitEdit(e) {
+        e.preventDefault();
+        props.editModal(props.notebook); 
     };
 
-    render() {
-        if(this.props.notebook.shortcut){
-            return (
-                <div className='notebook-drop-down-modal'>
-                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                    <br />
-                    <p>{this.props.notebook.name}</p>
-                    <br />
-                    <span onClick={this.handleSubmitDelete(this.props.notebook)}>
-                        <button type='submit'>Delete Notebook</button>
-                    </span>
-                    <br />
-                    <span onClick={this.handleSubmitEdit(this.props.notebook)}>
-                        <button type='submit'>Edit Notebook Name</button>
-                    </span>
-                    <br/>
-                    <span onClick={this.handleSubmitShortcut(this.props.notebook)}>
-                        <button type='submit'>Remove from Shortcuts</button>
-                    </span>
-                    <br />
-                </div>
-            )
+    function handleSubmitShortcut(e){
+        
+        e.preventDefault();
+        const name = props.notebook.name;
+        const user_id = props.notebook.user_id;
+        const shortcut = !props.notebook.shortcut;
+        const id = props.notebook.id;
+        props.closeModal();
+        props.updateNotebook({id: id, name: name, user_id: user_id, shortcut: shortcut})
+        
+    };
 
-        } else {
-            return (
-                <div className='notebook-drop-down-modal'>
-                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                    <br/>
-                    <p>{this.props.notebook.name}</p>
-                    <br/>
-                    <span onClick={this.handleSubmitDelete(this.props.notebook)}>
-                        <button type='submit'>Delete Notebook</button>
-                    </span>
-                    <br/>
-                    <span onClick={this.handleSubmitEdit(this.props.notebook)}>
-                        <button type='submit'>Edit Notebook Name</button>
-                    </span>
-                    
-                    <br/>
-                    <span onClick={this.handleSubmitShortcut(this.props.notebook)}>
-                        <button type='submits'>Add to Shortcuts</button>
-                    </span>
-                    <br />
-                </div>
-            )
+    
+        return(
+            
+                  
+            <div className='notebook-drop-down-modal'>
+                <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
+                <br/>
+                <p>{props.notebook.name}</p>
+                <br/>
+            
+                    <button onClick={handleSubmitDelete} type='submit'>Delete Notebook</button>
+                
+                <br/>
+                
+                    <button onClick={handleSubmitEdit} type='submit'>Edit Notebook Name</button>
 
-        }
-    }
+                
+                <br/>
+
+                <button onClick={handleSubmitShortcut} type='submit'>{props.notebook.shortcut ? Remove from Shortcuts : Add to Shortcuts}</button>
+        
+                <br />
+            </div>
+        )
+
+        
+    
 }
 
 export default NotebookDropDownForm;
