71fbc43117401b3b3ffcebdeec9a7c46605ba2f6
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 00:55:57 -0400

    convert notebook edit form to hook

--- frontend/components/notebooks/notebookEditForm.jsx
+++ frontend/components/notebooks/notebookEditForm.jsx
@@ -1,66 +1,44 @@
-import React from 'react';
-class NotebookEditForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {name: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange() {
-   
-        return (e) => {
-            this.setState({ name: e.target.value });
-        };
-    }
+import React, {useState} from 'react';
 
-    handleSubmit(e) {
+const NotebookEditForm = (props) => {
+    
+
+    const [newName, updateName] = useState(() => {
+        return '';
+    })
+
+    function handleChange(e) {
         e.preventDefault();
-        const name = this.state.name;
-        const user_id = this.props.user.id
-        const id = this.props.id;
-        this.props.updateNotebook({ name, user_id, id }).then(() => this.props.closeModal());
+        updateName(e.target.value);
     }
 
-    render (){
-        if (this.state.name.length < 1){
-            // non-working button to prevent empty submissions
-            return (
-                <div className="notebook-edit-modal">
-                    <div className="notebook-edit-top">
-                        <h1>Rename notebook</h1>
-                        <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                    </div>
-                    <h3>Name</h3>
-                    <span>
-
-                        <input type="text" value={this.state.name} onChange={this.handleChange()} />
-                    </span>
-
-                    <form>
-                        <button className="invalid"type='submit'>Done</button>
-                    </form>
-                </div>
-            )
-        } else {
-            return(
-                <div className="notebook-edit-modal">
-                    <div className="notebook-edit-top">
-                        <h1>Rename notebook</h1>
-                        <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                    </div>
-                    <h3>Name</h3>
-                    <span>
-    
-                        <input type="text" value={this.state.name} onChange={this.handleChange()}/>
-                    </span>
-    
-                    <form onSubmit={this.handleSubmit}>
-                        <button className="valid" type='submit'>Done</button>
-                    </form>
-                </div>
-            )
-        }
+    function handleSubmit(e) {
+        e.preventDefault();
+        const name = newName;
+        const user_id = props.user.id
+        const id = props.id;
+        props.updateNotebook({ name, user_id, id }).then(() => props.closeModal());
     }
+
+    return(
+        <div className="notebook-edit-modal">
+            <div className="notebook-edit-top">
+                <h1>Rename notebook</h1>
+                <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
+            </div>
+            <h3>Name</h3>
+            <span>
+
+                <input type="text" value={newName} onChange={handleChange}/>
+            </span>
+
+            {newName ? <button onClick={handleSubmit} className="valid" type='submit'>Done</button>  : ''}
+            
+            
+        </div>
+    )
+        
+
 }
 
 
