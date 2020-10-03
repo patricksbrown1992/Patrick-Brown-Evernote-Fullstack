e4a4eba7b0d8134c842b01d902002dfe85eae8c2
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 01:03:17 -0400

    converted note delete form to hook

--- frontend/components/notes/noteDeleteForm.jsx
+++ frontend/components/notes/noteDeleteForm.jsx
@@ -1,34 +1,29 @@
 import React from 'react';
 
-class NoteDeleteForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.handleSubmitDelete = this.handleSubmitDelete.bind(this);
-    }
-
-
-    handleSubmitDelete(e){
+const NoteDeleteForm = (props) =>  {
+    
+    function handleSubmitDelete(e){
         
         e.preventDefault();
-        this.props.deleteNote(this.props.note).then(() => this.props.closeModal())
+        props.deleteNote(props.note).then(() => props.closeModal())
 
     }
 
-    render(){
-        return (
-            <div className="note-delete-modal">
-                <div className = "note-delete-modal-top">
-                    <h1>Delete Note?</h1>
-                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                </div>
-                <h3>Are you sure you want to delete the "{this.props.note.title}" note?</h3>
-
-                <form onSubmit={this.handleSubmitDelete}>
-                    <button type='submit'>Delete</button>
-                </form>
+    
+    return (
+        <div className="note-delete-modal">
+            <div className = "note-delete-modal-top">
+                <h1>Delete Note?</h1>
+                <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
             </div>
-        )
-    }
+            <h3>Are you sure you want to delete the "{props.note.title}" note?</h3>
+
+            <form onSubmit={handleSubmitDelete}>
+                <button type='submit'>Delete</button>
+            </form>
+        </div>
+    )
+    
 }
 
 export default NoteDeleteForm;
