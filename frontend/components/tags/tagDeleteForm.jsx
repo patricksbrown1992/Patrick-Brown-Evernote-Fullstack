b2e10eaef7025c69314ae5cb6859243edf104c45
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 00:32:20 -0400

    converted tags new form to hook

--- frontend/components/tags/tagDeleteForm.jsx
+++ frontend/components/tags/tagDeleteForm.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
 
-class TagDeleteForm extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+const TagDeleteForm = (props) => {
 
-    handleSubmit(e) {
+    function handleSubmit(e) {
         e.preventDefault();
-        this.props.deleteTag(this.props.tag).then(() => this.props.closeModal());
-
+        props.deleteTag(props.tag).then(() => props.closeModal());
     }
 
-
-    render() {
-
-        return (
-            <div className="tag-delete-modal">
-                <div className="tag-delete-modal-top">
-                    <h1>Delete Tag?</h1>
-                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                </div>
-                <h3>Are you sure you want to delete the "{this.props.tag.name}" tag? This tag will be removed from all notes.</h3>
-             
-                <form onSubmit={this.handleSubmit}>
-                    <button type='submit'>Delete</button>
-                </form>
+    return (
+        <div className="tag-delete-modal">
+            <div className="tag-delete-modal-top">
+                <h1>Delete Tag?</h1>
+                <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
             </div>
-        )
-    }
+            <h3>Are you sure you want to delete the "{props.tag.name}" tag? This tag will be removed from all notes.</h3>
+            
+            <form onSubmit={handleSubmit}>
+                <button type='submit'>Delete</button>
+            </form>
+        </div>
+    )
+    
 }
 
 export default TagDeleteForm;
