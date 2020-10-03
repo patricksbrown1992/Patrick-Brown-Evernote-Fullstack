43c622100df7601f65cc82fba1ae043de5c0de54
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 01:18:00 -0400

    converted new note into hook

--- frontend/components/notes/noteNewForm.jsx
+++ frontend/components/notes/noteNewForm.jsx
@@ -1,84 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
-class NoteNewForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {title: '', body: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(field) {
+const NoteNewForm = (props) => {
     
-        return (e) => { 
-            this.setState({ [field]: e.target.value });
-        };
-    }
 
-    handleSubmit(e) {
+    const [title, updateTitle] = useState(() => {
+        return '';
+    })
+
+    const [body, updateBody] = useState(() => {
+        return '';
+    })
+
+    function handleChangeBody(e) {
         e.preventDefault();
-    
-        const title = this.state.title;
-        const notebook_id = this.props.id;
-        const id = this.props.id;
-        const body = this.state.body;
+        updateBody(e.target.value);
+    }
+
+    function handleChangeTitle(e) {
+        e.preventDefault();
+        updateTitle(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const notebook_id = props.id;
+        const id = props.id;
         const note = {title, body, notebook_id};
        
-        this.props.createNote({id, note}).then( () => this.props.closeModal());
+        props.createNote({id, note}).then( () => props.closeModal());
     }
 
 
        
-    render(){
-        if (this.state.title.length < 1 || this.state.body.length < 1){
-            // non-working button to prevent empty submissions
-            return (
-                <div className='new-note-modal'>
-                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                    <h1>Create new Note</h1>
+    
 
-                    <h5>Title</h5>
-                    <span>
-                        <input type="text" value={this.state.title} onChange={this.handleChange('title')} placeholder='Note Title' />
-                    </span>
-                    <h5>Body</h5>
-                    <span>
-                        <input type="text" value={this.state.body} onChange={this.handleChange('body')} placeholder='Note Body' />
-                    </span>
-                    <br />
-                    <form>
-                        <button className="invalid" type='submit'>Done</button>
+    return (
+        <div className='new-note-modal'>
+            <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
+            <h1>Create new Note</h1>
+            
+            <h5>Title</h5>
+            <span>
+                <input type="text" value={title} onChange={handleChangeTitle} placeholder='Note Title' />
+            </span>
+            <h5>Body</h5>
+            <span>
+                <input type="text" value={body} onChange={handleChangeBody} placeholder='Note Body' />
+            </span>
+            <br/>
+            
+            {body.length && title.length ? <button onSubmit={handleSubmit} className="valid" type='submit'>Done</button> : ''}
 
-                    </form>
+           
 
-                </div>
-            )
-        } else {
-
-            return (
-                <div className='new-note-modal'>
-                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                    <h1>Create new Note</h1>
-                   
-                    <h5>Title</h5>
-                    <span>
-                        <input type="text" value={this.state.title} onChange={this.handleChange('title')} placeholder='Note Title' />
-                    </span>
-                    <h5>Body</h5>
-                    <span>
-                        <input type="text" value={this.state.body} onChange={this.handleChange('body')} placeholder='Note Body' />
-                    </span>
-                    <br/>
-                    <form onSubmit={this.handleSubmit}>
-                        <button className="valid" type='submit'>Done</button>
-        
-                    </form>
-        
-                </div>
-                )
-        }
-    }
+        </div>
+    )
+      
 }
 
 export default NoteNewForm;
