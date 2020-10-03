356bd54010eb4e9fe06b8080659735a7f36a258a
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 00:43:57 -0400

    converted notebook delete form to hook

--- frontend/components/notebooks/notebookDeleteForm.jsx
+++ frontend/components/notebooks/notebookDeleteForm.jsx
@@ -1,43 +1,34 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-class NotebookDeleteForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+const NotebookDeleteForm = (props) =>  {
+    
 
-    handleSubmit(e) {
+    function handleSubmit(e) {
 
         e.preventDefault();
-        this.props.deleteNotebook(this.props.notebook)
-        .then( () => this.props.notes.forEach( (note) => {
-            if(note.notebook_id == this.props.notebook.id){
-                this.props.deleteNote(note)
-            }
-        }))
-        .then( () => this.props.closeModal());
-
-        
-       
+        props.deleteNotebook(props.notebook).then( () => props.notes.forEach( (note) => {
+            if(note.notebook_id == props.notebook.id){
+                props.deleteNote(note)
+            }})).then( () => props.closeModal());
     }
   
 
-    render() {
+    
 
-        return (
-            <div className="notebook-delete-modal">
-                <div className="noteboook-delete-top">
-                    <h1>Delete Notebook?</h1>
-                    <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                </div>
-                <h3>Are you sure you want to delete the "{this.props.notebook.name}" notebook?</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <button type='submit'>Delete</button>
-                </form>
+    return (
+        <div className="notebook-delete-modal">
+            <div className="noteboook-delete-top">
+                <h1>Delete Notebook?</h1>
+                <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
             </div>
-        )
-    }
+            <h3>Are you sure you want to delete the "{props.notebook.name}" notebook?</h3>
+            
+            <button onClick={handleSubmit} type='submit'>Delete</button>
+           
+        </div>
+    )
+    
 }
 
 export default NotebookDeleteForm;
