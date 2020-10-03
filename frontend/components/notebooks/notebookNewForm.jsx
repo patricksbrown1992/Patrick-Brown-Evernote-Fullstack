3ba9eb99236e4c0a04bef9404adaf828ba0c909f
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 00:59:56 -0400

    converted notebook new form to hook

--- frontend/components/notebooks/notebookNewForm.jsx
+++ frontend/components/notebooks/notebookNewForm.jsx
@@ -1,75 +1,47 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 
 
-class NotebookNewForm extends React.Component {
+const NotebookNewForm = (props) => {
 
-    constructor(props){
-        super(props);
-        this.state = {name: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    
+    const [newName, updateName] = useState(() => {
+        return '';
+    })
 
-    handleChange() {
-        
-        return (e) => {
-            this.setState({ name: e.target.value });
-        };
-    }
-
-    handleSubmit(e){
+    function handleChange(e) {
         e.preventDefault();
-        const name = this.state.name;
-        const user_id = Object.values(this.props.user)[0].id;
-       
-        this.props.createNotebook({name, user_id}).then( () => this.props.closeModal());
+        updateName(e.target.value);    
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const name = newName;
+        const user_id = Object.values(props.user)[0].id;
+        props.createNotebook({name, user_id}).then( () => props.closeModal());
     }
     
-
-    render (){
-        // non-working button to prevent empty submissions
-        if (this.state.name.length < 1){
-            return (
-                <div className='new-notebook-modal'>
-                    <div className='new-notebook-modal-top'>
-                        <h1>Create new notebook</h1>
-                        <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                    </div>
-                    <h3>Notebooks are useful for grouping notes around a common topic. They can be private or shared.</h3>
-                    <h5>Name</h5>
-                    <span>
-                        <input type="text" value={this.state.name} onChange={this.handleChange()} placeholder='Notebook name' />
-                    </span>
-                    <br />
-                    <br />
-                    <form >
-                        <button className="invalid" type='submit'>Done</button>
-                    </form>
+        return(
+            <div className='new-notebook-modal'>
+                <div className='new-notebook-modal-top'>
+                    <h1>Create new notebook</h1>
+                    <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
                 </div>
-            )
-        } else {
-            return(
-                <div className='new-notebook-modal'>
-                    <div className='new-notebook-modal-top'>
-                        <h1>Create new notebook</h1>
-                        <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                    </div>
-                    <h3>Notebooks are useful for grouping notes around a common topic. They can be private or shared.</h3>
-                    <h5>Name</h5>
-                    <span>
+                <h3>Notebooks are useful for grouping notes around a common topic. They can be private or shared.</h3>
+                <h5>Name</h5>
+                <span>
+
+                    <input type="text" value={newName} onChange={handleChange} placeholder='Notebook name'/>
+                </span>
+                <br/>
+                <br/>
+             
+                {newName ? <button onClick={handleSubmit} className="valid"  type='submit'>Done</button> : ''}
+             
+            </div>
+        )
+
+        
     
-                        <input type="text" value={this.state.name} onChange={this.handleChange()} placeholder='Notebook name'/>
-                    </span>
-                    <br/>
-                    <br/>
-                    <form onSubmit={this.handleSubmit}>
-                        <button className="valid"  type='submit'>Done</button>
-                    </form>
-                </div>
-            )
-
-        }
-    }
 }
 
 
