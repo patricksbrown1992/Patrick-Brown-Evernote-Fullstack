2a2b83439aeb5889019cac2cdb90be9f3ab78bc1
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-04 17:41:54 -0400

    converted notebook new form into hook

--- frontend/components/notebooks/notebookNewForm.jsx
+++ frontend/components/notebooks/notebookNewForm.jsx
@@ -20,25 +20,25 @@ const NotebookNewForm = (props) => {
         props.createNotebook({name, user_id}).then( () => props.closeModal());
     }
     
-        return(
-            <div className='new-notebook-modal'>
-                <div className='new-notebook-modal-top'>
-                    <h1>Create new notebook</h1>
-                    <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
-                </div>
-                <h3>Notebooks are useful for grouping notes around a common topic. They can be private or shared.</h3>
-                <h5>Name</h5>
-                <span>
-
-                    <input type="text" value={newName} onChange={handleChange} placeholder='Notebook name'/>
-                </span>
-                <br/>
-                <br/>
-             
-                {newName ? <button onClick={handleSubmit} className="valid"  type='submit'>Done</button> : ''}
-             
+    return(
+        <div className='new-notebook-modal'>
+            <div className='new-notebook-modal-top'>
+                <h1>Create new notebook</h1>
+                <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
             </div>
-        )
+            <h3>Notebooks are useful for grouping notes around a common topic. They can be private or shared.</h3>
+            <h5>Name</h5>
+            <span>
+
+                <input type="text" value={newName} onChange={handleChange} placeholder='Notebook name'/>
+            </span>
+            <br/>
+            <br/>
+            
+            {newName ? <button onClick={handleSubmit} className="valid"  type='submit'>Done</button> : ''}
+            
+        </div>
+    )
 
         
     
