13c8c60f9d0259f95b9744fea6494e79084d266b
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-20 01:25:27 -0400

    user can switch between notes and auto save on mouse leave

--- frontend/components/notes/noteShowForm.jsx
+++ frontend/components/notes/noteShowForm.jsx
@@ -1,160 +1,124 @@
-import React from 'react';
-import ReactQuill from 'react-quill';
-import { Redirect } from 'react-router-dom';
+import React, { useEffect, useState, useRef } from "react";
+import ReactQuill from "react-quill";
+import { Redirect } from "react-router-dom";
 
-class NoteShowForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: this.props.note.title,
-            body: this.props.note.body,
-            notebook_id: this.props.note.notebook_id,
-            id: this.props.note.id,
-            change: false,
-        }
-        
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleSubmitDropDown = this.handleSubmitDropDown.bind(this);
-        this.handleAddTag = this.handleAddTag.bind(this);
-        this.handleMouseOut = this.handleMouseOut.bind(this)
-    }
+const NoteShowForm = (props) => {
+  {
+    if (!props.notes[props.match.params.note_id])
+      return <Redirect to={`/notebooks`} />;
+  }
 
-    handleSubmitDropDown(entity) {
-        return (e) => {
-            e.preventDefault();
-            this.props.noteDropDown(entity)
-        };
-    }
+  const [body, updateBody] = useState(() => {
+    // return props.notes[props.match.params.note_id].body || props.note.body;
+    return props.note.body;
+  });
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-  
-        if (prevProps.note == undefined) {
-            this.setState({change: true})
+  // const prevProps = useRef();
+  useEffect(() => {
+    // updateBody();
+    updateBody(props.notes[props.match.params.note_id].body);
+  }, [props.match.params.note_id]);
 
-           
-        } else {
+  // const [change, updateChange] = useState(() => {
+  //   return false;
+  // });
 
-            if (prevProps.note.id !== this.props.note.id) {
-                this.setState({
-                    id: this.props.note.id,
-                    title: this.props.note.title,
-                    body: this.props.note.body,
-                    notebook_id: this.props.note.notebook_id
-                });
-            } 
-        }
-    }
+  // componentDidUpdate(prevProps, prevState, snapshot) {
 
-    handleAddTag(e){
-        e.preventDefault()
+  //     if (prevProps.note == undefined) {
+  //         setState({change: true})
 
-        this.props.noteTagAddModal(this.props.match.params.note_id)
-    }
+  //     } else {
 
-    handleChange(value) {
-        // let title = this.state.title;
-        // let body = value
-        // let notebook_id = this.state.notebook_id;
-        // let id = this.state.id;
-        // let note = { title, body, notebook_id, id };
-        this.setState({ body: value });
-            // , () => this.props.updateNote({ id, note })
-           
-    }
+  //         if (prevProps.note.id !== props.note.id) {
+  //             setState({
+  //                 id: props.note.id,
+  //                 title: props.note.title,
+  //                 body: props.note.body,
+  //                 notebook_id: props.note.notebook_id
+  //             });
+  //         }
+  //     }
+  // }
+  function handleAddTag(e) {
+    e.preventDefault();
+    props.noteTagAddModal(props.match.params.note_id);
+  }
 
+  function handleMouseOut(e) {
+    e.preventDefault();
 
+    const id = props.notes[props.match.params.note_id].id;
+    const note = { body, id };
+    props.updateNote({ id, note });
+  }
 
-    handleSubmit() {
-        return (e) => {
-            e.preventDefault();
-            let title = this.state.title;
-            let body = this.state.body;
-            let notebook_id = this.state.notebook_id;
-            let id = this.state.id;
-            let note = { title, body, notebook_id, id };
-            this.props.updateNote({ id, note })
-        };
-    }
+  if (props.search.length > 0) return <Redirect to="/allnotes" />;
 
-    
-    handleMouseOut(){
-        let title = this.state.title;
-        let body = this.state.body;
-        let notebook_id = this.state.notebook_id;
-        let id = this.state.id;
-        let note = { title, body, notebook_id, id };
-        this.props.updateNote({ id, note })
-        
-    }
+  // if (change) return <Redirect to={`/username/${notebook_id}`} />;
 
-    render() {
-        
-        if (this.props.search.length > 0) {
-            return <Redirect to='/allnotes' />;
-        } else {
-            if(this.state.change){
-                return <Redirect to={`/username/${this.state.notebook_id}`} />
-            }
-            let note = this.props.note;
-            let showtagbutton;
-            if (this.props.tags.length < 1) {
-                showtagbutton = ''
-            } else {
-                showtagbutton = <button className='add-tag-on-note-button' onClick={this.handleAddTag}>Add Tag</button>
-            }
-            
+  let showtagbutton;
+  if (props.tags.length < 1) {
+    showtagbutton = "";
+  } else {
+    showtagbutton = (
+      <button className="add-tag-on-note-button" onClick={handleAddTag}>
+        Add Tag
+      </button>
+    );
+  }
 
-            let modules = {
-                toolbar: [
-                    [{ 'header': [1, 3, false] }],
-                    ['bold', 'italic', 'underline', 'strike'],
-                    [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-                    ['image']
-                ],
-            };
+  const modules = {
+    toolbar: [
+      [{ header: [1, 3, false] }],
+      ["bold", "italic", "underline", "strike"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["image"],
+    ],
+  };
 
-            let formats = [
-                'header',
-                'bold', 'italic', 'underline', 'list', 'bullet', 
-                'indent', 'strike', 'blockquote',
-                'link', 'image'
-            ];
-            return (
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "list",
+    "bullet",
+    "indent",
+    "strike",
+    "blockquote",
+    "link",
+    "image",
+  ];
 
-                <>
-                
-                    <div className="right-nav">
-                        <div className='note-title-top'>
-                            <h1>{note.title}</h1>
-                            {showtagbutton}
+  return (
+    <>
+      <div className="right-nav">
+        <div className="note-title-top">
+          <h1>{props.note.title}</h1>
+          {showtagbutton}
+        </div>
+      </div>
 
-                        </div>
-                    </div>
-       
-                    <div className='quill-container' onMouseLeave={this.handleMouseOut}>
-                        <ReactQuill className={'react-quill-element'} 
-                        
-                        value={this.state.body} 
-                        formats={formats} 
-                        modules={modules} 
-                        onChange={this.handleChange} 
-                        theme="snow" 
-                        />
-               
-                    </div>
-                        
-                
-                    
-
-                </>
-                    
- 
-            );
-        }
-    }
-
-}
-
+      <div className="quill-container" onMouseLeave={handleMouseOut}>
+        <ReactQuill
+          className="react-quill-element"
+          value={body}
+          formats={formats}
+          modules={modules}
+          onChange={(e) => {
+            return updateBody(e);
+          }}
+          theme="snow"
+        />
+      </div>
+    </>
+  );
+};
 
 export default NoteShowForm;
