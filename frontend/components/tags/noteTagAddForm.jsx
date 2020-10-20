d47325996cb1ace547b99544ed5aeab13bbac28d
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-20 01:24:47 -0400

    removed state used previously when class comp

--- frontend/components/tags/noteTagAddForm.jsx
+++ frontend/components/tags/noteTagAddForm.jsx
@@ -1,103 +1,88 @@
-import React, {useEffect, useState} from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { merge } from 'lodash';
+import React, { useEffect, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { merge } from "lodash";
 
-const NoteTagAddForm = props =>  {
-   
+const NoteTagAddForm = (props) => {
+  const [newTag, updateTag] = useState(() => {
+    return "";
+  });
 
-    const [newTag, updateTag] = useState(()=> {
-        return '';
-    })
+  useEffect(() => {
+    props.getTags(props.user);
+  }, []);
 
-    useEffect(()=> {
-        props.getTags(props.user)
-    }, []);
+  function clickItem(entity) {
+    return (e) => {
+      e.preventDefault();
+      const tag_id = entity.id;
+      const note_id = props.note;
+      const user_id = props.user.id;
+      const tagging = { note_id, tag_id, user_id };
+      props.closeModal();
+      props.createTagging(tagging);
+    };
+  }
 
-    
-
-    function handleChange(e) {
-        e.preventDefault()
-        updateTag(e.target.value );
+  function duplicateArray(array) {
+    // deep dupes objects
+    let ans = [];
+    for (let i = 0; i < array.length; i++) {
+      let newObject = merge({}, array[i]);
+      ans.push(newObject);
     }
 
-    function clickItem(entity) {
-        return (e) => {
-            
-            e.preventDefault();
-            const tag_id = entity.id;
-            const note_id = props.note;
-            const user_id = props.user.id;
-            const tagging = { note_id, tag_id, user_id};
-            props.closeModal();
-            props.createTagging(tagging);
+    return ans;
+  }
+
+  function sortTags(tags) {
+    let newTags = duplicateArray(tags);
+    let sorted = false;
+
+    while (!sorted) {
+      sorted = true;
+      // bubble sort
+      for (let i = 0; i < newTags.length - 1; i++) {
+        let current = newTags[i];
+        let next = newTags[i + 1];
+        if (current.name.toUpperCase() > next.name.toUpperCase()) {
+          // Swaps if first element is after second in alphabet
+          sorted = false;
+          [newTags[i], newTags[i + 1]] = [newTags[i + 1], newTags[i]];
         }
+      }
     }
+    return newTags;
+  }
 
-    function duplicateArray(array) {
-        // deep dupes objects
-        let ans = [];
-        for (let i = 0; i < array.length; i++) {
-            let newObject = merge({}, array[i]);
-            ans.push(newObject);
-        }
-        
-        return ans;
-    }
+  if (props.tags.length < 1) return null;
 
-    function sortTags(tags) {
-        let newTags = duplicateArray(tags)
-        let sorted = false;
-        
-        while (!sorted) {
-            sorted = true;
-            // bubble sort
-            for (let i = 0; i < newTags.length - 1; i++) {
-                let current = newTags[i];
-                let next = newTags[i + 1];
-                if (current.name.toUpperCase() > next.name.toUpperCase()) {
-                    // Swaps if first element is after second in alphabet
-                    sorted = false;
-                    [newTags[i], newTags[i + 1]] = [newTags[i + 1], newTags[i]]
-                }
-            }
-        }
-        return newTags;
-    }
+  let tags = sortTags(props.tags);
 
-  
-       
-        
-        if (props.tags.length < 1) return null;
-    
-        let tags = sortTags(props.tags);
-        
-        tags = tags.filter(tag => (
-            tag.name.toUpperCase().includes(state.name.toUpperCase()))
-        )
-        tags = tags.map(tag => {
-            return (
-                <li key={tag.id}>
-                    <div className="tag-name-item" onClick={clickItem(tag)}>{tag.name}</div>
-                </li>
-            )
-        })
-
-
+  tags = tags.filter((tag) =>
+    tag.name.toUpperCase().includes(newTag.toUpperCase())
+  );
+  tags = tags.map((tag) => {
     return (
-        <div className="tag-search-modal">
-            <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
-            <input type="text" value={newTag} onChange={handleChange} placeholder='Add a tag' />
-            <ul>{tags}</ul>
+      <li key={tag.id}>
+        <div className="tag-name-item" onClick={clickItem(tag)}>
+          {tag.name}
         </div>
-    )
+      </li>
+    );
+  });
 
-
-
-    
-
-
-
-
-}
+  return (
+    <div className="tag-search-modal">
+      <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
+      <input
+        type="text"
+        value={newTag}
+        onChange={(e) => updateTag(e.target.value)}
+        placeholder="Add a tag"
+      />
+      <ul>{tags}</ul>
+    </div>
+  );
+};
 
 export default NoteTagAddForm;
