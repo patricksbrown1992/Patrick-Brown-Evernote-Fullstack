ee03e81e2a8811b73a5a425fb8042dddd84280b1
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-22 00:09:20 -0400

    converted tag index to hook

--- frontend/components/tags/tagsIndexForm.jsx
+++ frontend/components/tags/tagsIndexForm.jsx
@@ -1,178 +1,174 @@
-import React from 'react';
-import UsernameFormLeft from '../username/nonNoteLeftContainer';
-import { Redirect, Link } from 'react-router-dom';
-import { merge } from 'lodash';
+import React, { useState, useEffect } from "react";
+// import UsernameFormLeft from "../username/nonNoteLeftContainer";
+import UsernameFormLeft from "../username/usernameLeftContainer";
+import { Redirect, Link } from "react-router-dom";
+import { merge } from "lodash";
 
-class TagIndexForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {tag: '', selected: false};
-        this.handleSubmitNewTag = this.handleSubmitNewTag.bind(this);
-        this.handleSubmitDropDown = this.handleSubmitDropDown.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.sortTags = this.sortTags.bind(this);
-        this.duplicateArray = this.duplicateArray.bind(this);
-        this.handleTagClickTriage = this.handleTagClickTriage.bind(this);
-      
-    }
+const TagIndexForm = (props) => {
+  useEffect(() => {
+    props.removeTriage();
+    props.getTags(props.user).then(() => props.getTaggings(props.user));
+    return () => {
+      props.clearTags();
+    };
+  }, []);
 
-    componentDidMount() {
-        this.props.removeTriage();
-        this.props.getTags(this.props.user).then(() => this.props.getTaggings(this.props.user));
-    }
+  const [selected, updateSelected] = useState(() => {
+    return false;
+  });
 
-    componentWillUnmount(){
-        this.props.clearTags();
-    }
+  const [search, updateSearch] = useState(() => {
+    return "";
+  });
 
-    handleTagClickTriage(entity){
-        return (e) => {
-            e.preventDefault()
-            this.setState({ selected: entity }, () => this.props.receiveTriage(entity)) 
+  function handleTagClickTriage(entity) {
+    return (e) => {
+      e.preventDefault();
+      updateSelected(entity);
+      props.receiveTriage(entity);
+    };
+  }
+
+  function handleClickNewTag(e) {
+    e.preventDefault();
+    props.addTag(props.user);
+  }
+
+  function sortTags(tags) {
+    let newTags = duplicateArray(tags);
+    let sorted = false;
+
+    while (!sorted) {
+      sorted = true;
+      // bubble sort
+      for (let i = 0; i < newTags.length - 1; i++) {
+        let current = newTags[i];
+        let next = newTags[i + 1];
+        if (current.name.toUpperCase() > next.name.toUpperCase()) {
+          // Swaps if first element is after second in alphabet
+          sorted = false;
+          [newTags[i], newTags[i + 1]] = [newTags[i + 1], newTags[i]];
         }
+      }
     }
-
-
-    handleSubmitNewTag(entity) {
-        return (e) => {
-            e.preventDefault();
-            this.props.addTag(entity);
-        };
-    }
-
-    handleChange() {
-        return (e) => {
-            this.setState({ tag: e.target.value })
-        }
-    }
-
-    sortTags(tags){
-        let newTags = this.duplicateArray(tags)
-        let sorted = false;
-        
-        while(!sorted){
-            sorted = true;
-            // bubble sort
-            for(let i = 0; i < newTags.length - 1; i++){
-                let current = newTags[i];
-                let next = newTags[i+1];
-                if (current.name.toUpperCase() > next.name.toUpperCase()){
-                    // Swaps if first element is after second in alphabet
-                    sorted = false;
-                    [newTags[i], newTags[i + 1]] = [newTags[i + 1], newTags[i]]
-                }
-            }
-
-        }
-        for(let i = 0; i < newTags.length; i++){
-            if(i === 0){
-                newTags[i].duplicate = false;
-            } else {
-                if (newTags[i].name[0].toUpperCase() == newTags[i-1].name[0].toUpperCase()){
-                    newTags[i].duplicate = true;
-                } else {
-                    newTags[i].duplicate = false;
-                }
-            }
-        }
-        return newTags;
-    }
-
-    duplicateArray(array){
-        // deep dupes objects
-        let ans = [];
-        for(let i = 0; i < array.length; i++){
-            let newObject = merge({}, array[i]);
-            ans.push(newObject);
-        }
-        
-        return ans;
-    }
-
-
-    handleSubmitDropDown(entity){
-        return(e) => {
-            e.preventDefault();
-            this.props.tagDropDown(entity);
-        }
-    }
-
-
-    render() {
-        // first render or no tags
-        if (this.props.search.length > 0) {
-            return <Redirect to='/allnotes' />;
+    for (let i = 0; i < newTags.length; i++) {
+      if (i === 0) {
+        newTags[i].duplicate = false;
+      } else {
+        if (
+          newTags[i].name[0].toUpperCase() ==
+          newTags[i - 1].name[0].toUpperCase()
+        ) {
+          newTags[i].duplicate = true;
         } else {
-
-            if (this.state.selected){
-                return <Redirect to='/allnotes' />
-            } else {
-                let tags
-                if (this.props.tags.length < 1) {
-                    return (
-                        <div className="tag-index">
-                            <UsernameFormLeft />
-                            <div className="tag-index-right">
-                                <div className="tag-index-upper">
-                                    <div className="tag-index-header">
-                                        <h1>Tags</h1>
-                                        <span onClick={this.handleSubmitNewTag(this.props.user)} className="new-tag"><i className="fas fa-plus-circle"></i><button type='submit'><p>New Tag</p></button></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                } else {
-                 
-                    tags = this.sortTags(this.props.tags);
-                    tags = tags.filter(tag => (
-                        tag.name.toUpperCase().includes(this.state.tag.toUpperCase()))
-                    )   
-             
-                    tags = tags.map(tag => {
-                        if(tag.duplicate){
-                            // if tag starts with same letter as one before it
-                            return (
-                                <li key={tag.id}>
-                                    {/* <div className="tag-index-intial">{tag.name[0]}</div> */}
-                                    <div className="tag-name"> <button className = "tag-triage-redirect-button" onClick={this.handleTagClickTriage(tag)}>{tag.name}</button><button onClick={this.handleSubmitDropDown(tag)} className='chevron-down-button'><i className="fas fa-chevron-down"></i></button></div>
-                                </li>
-                            )
-                        } else {
-                            return (
-                            <li key={tag.id}>
-                                <div className="tag-index-intial">{tag.name[0].toUpperCase()}</div>
-                                <div className="tag-name"><button className="tag-triage-redirect-button" onClick={this.handleTagClickTriage(tag)}>{tag.name}</button><button onClick={this.handleSubmitDropDown(tag)} className='chevron-down-button'><i className="fas fa-chevron-down"></i></button></div>
-                            </li>
-                        )
-                        }
-                    })
-    
-                    return (
-                        <div className="tag-index">
-                            <UsernameFormLeft />
-                            <div className="tag-index-right">
-                                <div className="tag-index-upper">
-                                    <div className="tag-index-header">
-                                        <h1>Tags</h1>
-                                        <input placeholder='Find Tags...' type="text" onChange={this.handleChange()} />
-                                    </div>
-                                    <div className="tag-index-add">
-                                        <span onClick={this.handleSubmitNewTag(this.props.user)} className="new-tag"><button type='submit'><i className="fas fa-plus-circle"></i><p>New Tag</p></button></span>
-                                    </div>
-                                </div>
-                                
-                                
-                                <div className="tag-index-list">
-                                    <ul>{tags}</ul>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
-            }      
+          newTags[i].duplicate = false;
         }
+      }
     }
-}
+    return newTags;
+  }
+
+  function duplicateArray(array) {
+    // deep dupes objects
+    let ans = [];
+    for (let i = 0; i < array.length; i++) {
+      let newObject = merge({}, array[i]);
+      ans.push(newObject);
+    }
+
+    return ans;
+  }
+
+  function handleClickDropDown(entity) {
+    return (e) => {
+      e.preventDefault();
+      props.tagDropDown(entity);
+    };
+  }
+
+  // first render or no tags
+  if (props.search.length > 0) return <Redirect to="/allnotes" />;
+
+  if (selected) return <Redirect to="/allnotes" />;
+
+  let tags = sortTags(props.tags);
+  tags = tags.filter((tag) =>
+    tag.name.toUpperCase().includes(search.toUpperCase())
+  );
+
+  tags = tags.map((tag) => {
+    if (tag.duplicate) {
+      // if tag starts with same letter as one before it
+      return (
+        <li key={tag.id}>
+          {/* <div className="tag-index-intial">{tag.name[0]}</div> */}
+          <div className="tag-name">
+            {" "}
+            <button
+              className="tag-triage-redirect-button"
+              onClick={handleTagClickTriage(tag)}
+            >
+              {tag.name}
+            </button>
+            <button
+              onClick={handleClickDropDown(tag)}
+              className="chevron-down-button"
+            >
+              <i className="fas fa-chevron-down"></i>
+            </button>
+          </div>
+        </li>
+      );
+    } else {
+      return (
+        <li key={tag.id}>
+          <div className="tag-index-intial">{tag.name[0].toUpperCase()}</div>
+          <div className="tag-name">
+            <button
+              className="tag-triage-redirect-button"
+              onClick={handleTagClickTriage(tag)}
+            >
+              {tag.name}
+            </button>
+            <button
+              onClick={handleClickDropDown(tag)}
+              className="chevron-down-button"
+            >
+              <i className="fas fa-chevron-down"></i>
+            </button>
+          </div>
+        </li>
+      );
+    }
+  });
+
+  return (
+    <div className="tag-index">
+      <UsernameFormLeft />
+
+      <div className="tag-index-right">
+        <div className="tag-index-upper">
+          <div className="tag-index-header">
+            <h1>Tags</h1>
+            <input
+              placeholder="Find Tags..."
+              type="text"
+              onChange={(e) => updateSearch(e.target.value)}
+            />
+          </div>
+          <div className="tag-index-add">
+            <button onClick={handleClickNewTag} className="new-tag">
+              <i className="fas fa-plus-circle"></i> New Tag
+            </button>
+          </div>
+        </div>
+
+        <div className="tag-index-list">
+          <ul>{tags}</ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TagIndexForm;
