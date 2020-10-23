6e5fa26389aa8a4dc389ac6b4446100771e72935
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-23 02:49:09 -0400

    turned tag search to hook

--- frontend/components/tags/tagSearchForm.jsx
+++ frontend/components/tags/tagSearchForm.jsx
@@ -1,105 +1,85 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { merge } from 'lodash';
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { merge } from "lodash";
 
-class TagSearchForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { name: '' };
-        this.handleChange = this.handleChange.bind(this);
-        this.duplicateArray = this.duplicateArray.bind(this);
-        this.sortTags = this.sortTags.bind(this);
-        this.clickItem = this.clickItem.bind(this);
+const TagSearchForm = (props) => {
+  const [search, updateSearch] = useState(() => {
+    return "";
+  });
+
+  useEffect(() => {
+    props.getTags(props.user);
+  }, []);
+
+  function clickItem(entity) {
+    return (e) => {
+      e.preventDefault();
+      props.closeModal();
+      props.removeTriage();
+      props.updateTriage(entity);
+    };
+  }
+
+  function duplicateArray(array) {
+    // deep dupes objects
+    let ans = [];
+    for (let i = 0; i < array.length; i++) {
+      let newObject = merge({}, array[i]);
+      ans.push(newObject);
     }
 
-    componentDidMount(){
-        this.props.getTags(this.props.user)
-    }
+    return ans;
+  }
 
-    handleChange() {
-        return (e) => {
-            this.setState({ name: e.target.value });
-        };
-    }
-    
+  function sortTags(tags) {
+    let newTags = duplicateArray(tags);
+    let sorted = false;
 
-    clickItem(entity){
-        return(e) => {
-            e.preventDefault();
-            // entity.activated = !entity.activated;
-            // this.props.closeModal();
-            this.props.closeModal();
-            this.props.removeTriage();
-            this.props.updateTriage(entity)
-
+    while (!sorted) {
+      sorted = true;
+      // bubble sort
+      for (let i = 0; i < newTags.length - 1; i++) {
+        let current = newTags[i];
+        let next = newTags[i + 1];
+        if (current.name.toUpperCase() > next.name.toUpperCase()) {
+          // Swaps if first element is after second in alphabet
+          sorted = false;
+          [newTags[i], newTags[i + 1]] = [newTags[i + 1], newTags[i]];
         }
+      }
     }
+    return newTags;
+  }
 
-    duplicateArray(array) {
-        // deep dupes objects
-        let ans = [];
-        for (let i = 0; i < array.length; i++) {
-            let newObject = merge({}, array[i]);
-            ans.push(newObject);
-        }
-        
-        return ans;
-    }
+  if (!props.tags.length) return null;
 
-    sortTags(tags) {
-        let newTags = this.duplicateArray(tags)
-        let sorted = false;
-        
-        while (!sorted) {
-            sorted = true;
-            // bubble sort
-            for (let i = 0; i < newTags.length - 1; i++) {
-                let current = newTags[i];
-                let next = newTags[i + 1];
-                if (current.name.toUpperCase() > next.name.toUpperCase()) {
-                    // Swaps if first element is after second in alphabet
-                    sorted = false;
-                    [newTags[i], newTags[i + 1]] = [newTags[i + 1], newTags[i]]
-                }
-            }
-        }
-        return newTags;
-    }
+  let tags = sortTags(props.tags);
 
-    render(){
-        let tags;
-        if(this.props.tags.length < 1){
-            return null;
-        }
-        tags = this.sortTags(this.props.tags);
-        
-        tags = tags.filter(tag => (
-            tag.name.toUpperCase().includes(this.state.name.toUpperCase()))
-        )   
-        tags = tags.map(tag => {
-            return (
-                <li key={tag.id}> 
-                    <div className="tag-name-item" onClick={this.clickItem(tag)}>{tag.name}</div>
-                </li>
-            )
-        })
+  tags = tags.filter((tag) =>
+    tag.name.toUpperCase().includes(search.toUpperCase())
+  );
+  tags = tags.map((tag) => {
+    return (
+      <li key={tag.id}>
+        <div className="tag-name-item" onClick={clickItem(tag)}>
+          {tag.name}
+        </div>
+      </li>
+    );
+  });
 
-
-        return(
-            <div className="tag-search-modal">
-                <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
-                <input type="text" value={this.state.name} onChange={this.handleChange()} placeholder='Find tags...' />
-                <ul>{tags}</ul>
-            </div>
-        )
-
-
-
-    }
-
-
-
-
-}
+  return (
+    <div className="tag-search-modal">
+      <i onClick={props.closeModal} className="fas fa-times fa-2x"></i>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => updateSearch(e.target.value)}
+        placeholder="Find tags..."
+      />
+      <ul>{tags}</ul>
+    </div>
+  );
+};
 
 export default TagSearchForm;
