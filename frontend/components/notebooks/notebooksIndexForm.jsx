bb4ae82d62180e1d3d70990e39788d6b8be2a830
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-23 22:30:52 -0400

    properly styled notebook rows in table

--- frontend/components/notebooks/notebooksIndexForm.jsx
+++ frontend/components/notebooks/notebooksIndexForm.jsx
@@ -1,312 +1,181 @@
-import React from 'react';
-import UsernameFormLeft from '../username/nonNoteLeftContainer';
-import { merge } from 'lodash';
-import { Link, Redirect } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+// import UsernameFormLeft from "../username/nonNoteLeftContainer";
+import UsernameFormLeft from "../username/usernameLeftContainer";
+import { merge } from "lodash";
+import { Link, Redirect } from "react-router-dom";
+import styleDate from "../../util/styleDate";
 
+const NotebooksIndexForm = (props) => {
+  const [selected, updateSelected] = useState(() => {
+    return false;
+  });
 
-import styleDate from '../../util/styleDate';
+  const [body, updateBody] = useState(() => {
+    return "";
+  });
 
-class NotebooksIndexForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { loaded: false, selected: false, body: '', title: false};
-        this.updateSelected = this.updateSelected.bind(this);
-        this.handleSubmitAdd = this.handleSubmitAdd.bind(this);
-        this.handleSubmitDropDown = this.handleSubmitDropDown.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmitNoteDropDown = this.handleSubmitNoteDropDown.bind(this);
-        this.handleSort = this.handleSort.bind(this);
-        this.duplicateArray = this.duplicateArray.bind(this);
-        this.handleTitleClick = this.handleTitleClick.bind(this);
-        this.handleSortNotes = this.handleSortNotes.bind(this);
-        // this.onlyCorrectNotes = this.onlyCorrectNotes.bind(this);
+  const [title, updateTitle] = useState(() => {
+    return false;
+  });
 
+  useEffect(() => {
+    props.getNotebooks(props.user);
+  }, []);
+
+  function handleClickAdd(e) {
+    e.preventDefault();
+    props.addModal();
+  }
+
+  function handleTitleClick(e) {
+    e.preventDefault();
+    updateTitle(!title);
+    handleSort(props.notebooks);
+  }
+
+  function handleSort(notebooks) {
+    let newNotebooks = duplicateArray(notebooks);
+    if (title === true) {
+      let sorted = false;
+      while (!sorted) {
+        sorted = true;
+        // bubble sort
+        for (let i = 0; i < newNotebooks.length - 1; i++) {
+          let current = newNotebooks[i];
+          let next = newNotebooks[i + 1];
+          if (current.name.toUpperCase() < next.name.toUpperCase()) {
+            // Swaps if first element is before the second in alphabet
+            sorted = false;
+            [newNotebooks[i], newNotebooks[i + 1]] = [
+              newNotebooks[i + 1],
+              newNotebooks[i],
+            ];
+          }
+        }
+      }
+      return newNotebooks;
+    } else {
+      let sorted = false;
+      while (!sorted) {
+        sorted = true;
+        // bubble sort
+        for (let i = 0; i < newNotebooks.length - 1; i++) {
+          let current = newNotebooks[i];
+          let next = newNotebooks[i + 1];
+          if (current.name.toUpperCase() > next.name.toUpperCase()) {
+            // Swaps if first element is after the second in alphabet
+            sorted = false;
+            [newNotebooks[i], newNotebooks[i + 1]] = [
+              newNotebooks[i + 1],
+              newNotebooks[i],
+            ];
+          }
+        }
+      }
+      return newNotebooks;
     }
+  }
 
-
-    componentDidMount() {
-        this.props.getNotebooks(this.props.user).then(() => this.props.notebooks.forEach((notebook) => {
-            this.props.getNotes(notebook.id)
-        }));
+  function duplicateArray(array) {
+    // deep dupes objects
+    let ans = [];
+    for (let i = 0; i < array.length; i++) {
+      let newObject = merge({}, array[i]);
+      ans.push(newObject);
     }
+    return ans;
+  }
 
-    handleSubmitAdd(e) {
+  function handleClickDropDown(entity) {
+    return (e) => {
+      e.preventDefault();
+      props.notebookDropDown(entity);
+    };
+  }
+
+  function changeSelected(id) {
+    if ((selected === 0 || selected) && selected === id) {
+      return (e) => {
         e.preventDefault();
-
-        this.props.addModal();
+        updateSelected(false);
+      };
+    } else {
+      return (e) => {
+        e.preventDefault();
+        updateSelected(id);
+      };
     }
-    
+  }
+  if (props.search.length > 0) return <Redirect to="/allnotes" />;
+  let notebooks;
+  let arrow;
 
-    handleChange() {
-        return (e) => {
-            this.setState({ body: e.target.value })  
-        }
-    }
+  notebooks = handleSort(props.notebooks);
+  notebooks = notebooks.filter((notebook) =>
+    notebook.name.toUpperCase().includes(body.toUpperCase())
+  );
 
-    handleTitleClick(){
-        this.setState({ title: !this.state.title }, () => this.handleSort(this.props.notebooks));
-    }
+  notebooks = notebooks.map((notebook) => {
+    return (
+      <tr key={notebook.id}>
+        <td>
+          <Link to={`/username/${notebook.id}`}>{notebook.name}</Link>
+        </td>
+        <td>{props.user.email}</td>
+        <td>{styleDate(notebook.updated_at)}</td>
+        <td>
+          <i
+            onClick={handleClickDropDown(notebook)}
+            className="fas fa-ellipsis-h"
+          ></i>
+        </td>
+      </tr>
+    );
+  });
+  if (title) {
+    arrow = "fas fa-arrow-up";
+  } else {
+    arrow = "fas fa-arrow-down";
+  }
 
- 
+  return (
+    <div className="notebooks-index">
+      <UsernameFormLeft />
 
-    handleSort(notebooks){
-        let newNotebooks = this.duplicateArray(notebooks);
-        if(this.state.title === true){
-            let sorted = false;
-            while (!sorted) {
-                sorted = true;
-                // bubble sort
-                for (let i = 0; i < newNotebooks.length - 1; i++) {
-                    let current = newNotebooks[i];
-                    let next = newNotebooks[i + 1];
-                    if (current.name.toUpperCase() < next.name.toUpperCase()) {
-                        // Swaps if first element is before the second in alphabet
-                        sorted = false;
-                        [newNotebooks[i], newNotebooks[i + 1]] = [newNotebooks[i + 1], newNotebooks[i]]
-                    }
-                }
-            }
-            return newNotebooks;
-        } else {            
-            let sorted = false;
-            while (!sorted) {
-                sorted = true;
-                // bubble sort
-                for (let i = 0; i < newNotebooks.length - 1; i++) {
-                    let current = newNotebooks[i];
-                    let next = newNotebooks[i + 1];
-                    if (current.name.toUpperCase() > next.name.toUpperCase()) {
-                        // Swaps if first element is after the second in alphabet
-                        sorted = false;
-                        [newNotebooks[i], newNotebooks[i + 1]] = [newNotebooks[i + 1], newNotebooks[i]]
-                    }
-                }
-            }
-            return newNotebooks;
-        }
-    }
+      <div className="notebooks-index-right">
+        <div className="notebook-index-top-div">
+          <div className="notebook-index-top">
+            <h1>Notebooks</h1>
+            <input
+              placeholder="Find Notebooks..."
+              value={body}
+              type="text"
+              onChange={(e) => updateBody(e.target.value)}
+            />
+          </div>
+          <div className="notebook-index-header">
+            <h3>My notebook list</h3>
 
-    handleSortNotes(notes){
-        // let newNotes = this.duplicateArray(notes);
-        if (this.state.title === true) {      
-            let sorted = false;
-            while (!sorted) {
-                sorted = true;
-                // bubble sort
-                for (let i = 0; i < notes.length - 1; i++) {
-                    let current = notes[i];
-                    let next = notes[i + 1];
-                    if (current.title.toUpperCase() < next.title.toUpperCase()) {
-                        // Swaps if first element is before the second in alphabet
-                        sorted = false;
-                        [notes[i], notes[i + 1]] = [notes[i + 1], notes[i]]
-                    }
-                }
-            }
-            return notes;
+            <button className="new-notebook-button" onClick={handleClickAdd}>
+              <i className="fas fa-book-medical"></i>New Notebook
+            </button>
+          </div>
+        </div>
+        <table>
+          <tr style={{ marginBottom: "15px" }}>
+            <th style={{ cursor: "pointer" }} onClick={handleTitleClick}>
+              Title <i className={arrow}></i>
+            </th>
+            <th>Create By</th>
+            <th>Updated</th>
+            <th>Actions</th>
+          </tr>
 
-        } else {
-            let sorted = false;
-            while (!sorted) {
-                sorted = true;
-                // bubble sort
-                for (let i = 0; i < notes.length - 1; i++) {
-                    let current = notes[i];
-                    let next = notes[i + 1];
-                    if (current.title.toUpperCase() > next.title.toUpperCase()) {
-                        // Swaps if first element is after the second in alphabet
-                        sorted = false;
-                        [notes[i], notes[i + 1]] = [notes[i + 1], notes[i]]
-                    }
-                }
-            }
-            return notes;
-
-        }
-    }
-
-    
-
-    duplicateArray(array){
-        // deep dupes objects
-        let ans = [];
-        for (let i = 0; i < array.length; i++) {
-            let newObject = merge({}, array[i]);
-            ans.push(newObject);
-        }
-        return ans;
-    }
-
-    handleSubmitDropDown(entity) {
-        return (e) => {
-            e.preventDefault();
-            this.props.notebookDropDown(entity)
-        };
-    }
-
-    handleSubmitNoteDropDown(entity){
-        return (e) => {
-            e.preventDefault();
-            this.props.noteDropDown(entity)
-        };
-    }
-
-
-    updateSelected(id) {
-        if((this.state.selected === 0 || this.state.selected) && this.state.selected === id){
-            return (e) => {
-                e.preventDefault();
-                // this.props.clearNotes();
-                this.setState({ selected: false })
-            };
-        } else {
-            return (e) => {
-                e.preventDefault();
-                // this.props.clearNotes();
-                // this.setState({ selected: id }, () => this.props.getNotes(this.state.selected));
-                this.setState({selected: id})
-            };
-        }
-    
-    }
-
-    render() {
-        if (this.props.search.length > 0) {
-            return <Redirect to='/allnotes' />;
-        } else {
-        
-            let notebooks;
-            let caret;
-            let arrow;
-            if (this.props.notebooks.length < 1) {
-                return (
-                    <div className='notebooks-index'>
-
-                        <UsernameFormLeft />
-
-                        <div className='notebooks-index-right'>
-                            <div className="notebook-index-top-div">
-                                <div className="notebook-index-top">
-                                    <h1>Notebooks</h1>
-                                    <input placeholder='Find Notebooks...' type="text" value={this.state.body} onChange={this.handleChange()} />
-                                </div>
-                                <div className="notebook-index-header">
-
-
-                                    <h3>My notebook list</h3>
-                                    <form onSubmit={this.handleSubmitAdd}>
-
-                                        <button className='new-notebook-button' type='submit'><i className="fas fa-book-medical"></i>New Notebook</button>
-                                    </form>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                );
-            } else {
-                notebooks = this.handleSort(this.props.notebooks);
-                notebooks = notebooks.filter(notebook => (
-                    notebook.name.toUpperCase().includes(this.state.body.toUpperCase()))
-                )   
-            
-                notebooks = notebooks.map(notebook => {
-                    let selectedNotebook;
-                    let notes;
-                    
-                    if(notebook.id === this.state.selected){
-                        selectedNotebook = 'selectedNotebook';
-                        caret = "fas fa-caret-down";
-                        // notes = this.onlyCorrectNotes(this.props.notes, notebook.id);
-                        notes = this.handleSortNotes(this.props.notes);
-                        notes = notes.map(note => {
-                            if(note.notebook_id === notebook.id){
-                            
-                                return(
-                                <li key={note.id} className="notebook-note-index-item">
-                                    <div className="notebook-index-note-title"><i className="fas fa-sticky-note"></i><Link to={`/username/${note.notebook_id}/notes/${note.id}`}>{note.title}</Link></div>
-                                    <div className="notebook-index-note-email"><h4>{this.props.user.email}</h4></div>
-                                    <div className="notebook-index-note-time"><h4>{styleDate(note.updated_at)}</h4></div>
-                                    {/* <div className="notebook-index-table-button"><button onClick={this.handleSubmitEdit(notebook)} type='submit'><i className="fas fa-ellipsis-h"></i>Rename Notebook</button></div> */}
-                                    <div className="notebook-index-note-button"><i onClick={this.handleSubmitNoteDropDown(note)} className="fas fa-ellipsis-h"></i></div>
-                                    {/* <i class="fas fa-caret-down"></i> */}
-                                </li>
-                                )
-                            }
-                        });
-                    } else {
-                        selectedNotebook = 'notebook-index-table';
-                        notes = '';
-                        caret = "fas fa-caret-right";
-                    }
-                    if(this.state.title){
-                        arrow = "fas fa-arrow-up"
-                    } else {
-                        arrow = "fas fa-arrow-down"
-                    }
-
-                    return (
-                    <div>
-                            <li key={notebook.id} className={selectedNotebook} >
-                                <div className="notebook-index-table-title"> <i onClick={this.updateSelected(notebook.id)} className={caret}></i> <Link to={`/username/${notebook.id}`}><i className="fas fa-book"></i>{notebook.name}</Link></div>
-                                <div className="notebook-index-table-email"><h4>{this.props.user.email}</h4></div>
-                                <div className="notebook-index-table-time"><h4>{styleDate(notebook.updated_at)}</h4></div>
-                                {/* <div className="notebook-index-table-button"><button onClick={this.handleSubmitEdit(notebook)} type='submit'><i className="fas fa-ellipsis-h"></i>Rename Notebook</button></div> */}
-                                <div className="notebook-index-table-button"><i onClick={this.handleSubmitDropDown(notebook)} className="fas fa-ellipsis-h"></i></div>
-                                {/* <i class="fas fa-caret-down"></i> */}
-                            
-                            </li>
-                            <ul className="notebook-note-index">
-
-                                {notes}
-                            </ul>
-
-                    </div>
-                )})
-
-                return (
-
-                    <div className='notebooks-index'>
-
-                        <UsernameFormLeft />
-
-                        <div className='notebooks-index-right'>
-                            <div className= "notebook-index-top-div">
-                                <div className="notebook-index-top">
-                                    <h1>Notebooks</h1>
-                                    <input placeholder='Find Notebooks...' value={this.state.body} type="text" onChange={this.handleChange()}/>
-                                </div>
-                                <div className="notebook-index-header">
-                                
-                                
-                                    <h3>My notebook list</h3>
-                                    <form onSubmit={this.handleSubmitAdd}>
-
-                                        <button className='new-notebook-button' type='submit'><i className="fas fa-book-medical"></i>New Notebook</button>
-                                    </form>
-                                </div>
-
-                            </div>
-                            <div className='index-of-notebooks'>
-                                <div className='notebook-table-labs'>
-                                    <h6 onClick={this.handleTitleClick} className="notebook-table-label-title">Title <i className={arrow}></i></h6>
-                                    <h6 className="notebook-table-label-email">Create By</h6>
-                                    <h6 className="notebook-table-label-time">Updated</h6>
-                                    <h6 className="notebook-table-label-actions">Actions</h6>
-                                </div>
-                                {/* <ul className='notebooks-index-list'>{notebooks}</ul> */}
-                                <ul>{notebooks}</ul>
-                            </div>
-
-
-                        </div>
-                    </div>
-                );
-            }
-        }
-    }
-
-}
+          {notebooks}
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default NotebooksIndexForm;
