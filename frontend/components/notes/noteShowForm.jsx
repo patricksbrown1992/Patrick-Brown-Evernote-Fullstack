6bacaa2675e6209fc302c783f84726d33e48dd9d
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-27 02:05:52 -0400

    cleaned up note show form

--- frontend/components/notes/noteShowForm.jsx
+++ frontend/components/notes/noteShowForm.jsx
@@ -9,37 +9,13 @@ const NoteShowForm = (props) => {
   }
 
   const [body, updateBody] = useState(() => {
-    // return props.notes[props.match.params.note_id].body || props.note.body;
     return props.note.body;
   });
 
-  // const prevProps = useRef();
   useEffect(() => {
-    // updateBody();
     updateBody(props.notes[props.match.params.note_id].body);
   }, [props.match.params.note_id]);
 
-  // const [change, updateChange] = useState(() => {
-  //   return false;
-  // });
-
-  // componentDidUpdate(prevProps, prevState, snapshot) {
-
-  //     if (prevProps.note == undefined) {
-  //         setState({change: true})
-
-  //     } else {
-
-  //         if (prevProps.note.id !== props.note.id) {
-  //             setState({
-  //                 id: props.note.id,
-  //                 title: props.note.title,
-  //                 body: props.note.body,
-  //                 notebook_id: props.note.notebook_id
-  //             });
-  //         }
-  //     }
-  // }
   function handleAddTag(e) {
     e.preventDefault();
     props.noteTagAddModal(props.match.params.note_id);
@@ -55,18 +31,14 @@ const NoteShowForm = (props) => {
 
   if (props.search.length > 0) return <Redirect to="/allnotes" />;
 
-  // if (change) return <Redirect to={`/username/${notebook_id}`} />;
-
-  let showtagbutton;
-  if (props.tags.length < 1) {
-    showtagbutton = "";
-  } else {
-    showtagbutton = (
+  let showtagbutton =
+    props.tags.length < 1 ? (
+      ""
+    ) : (
       <button className="add-tag-on-note-button" onClick={handleAddTag}>
         Add Tag
       </button>
     );
-  }
 
   const modules = {
     toolbar: [
